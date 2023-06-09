cc0753e77a2671bd0f92a048d560bc7dc5df9912
David Velez <evelez@eluniverso.com>
2023-06-08 20:20:05 -0500

    se crean primeros endpoint para reservar habitaciones

---
+++ src/api/reservacion/services/reservacion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * reservacion service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::reservacion.reservacion');
